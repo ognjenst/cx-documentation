a55b601a4035553221f3b20fb9c5a199f7162ff6
Ognjen Stefanovic <ognjen.stefanovic@codaxy.com>
2022-03-14 16:30:28 +0100

    Home page

---
+++ app/routes/HomePage.js
@@ -0,0 +1,10 @@
+import { createFunctionalComponent } from 'cx/ui';
+
+export default () =>
+   createFunctionalComponent(() => {
+      return (
+         <cx>
+            <div>Hello</div>
+         </cx>
+      );
+   });
